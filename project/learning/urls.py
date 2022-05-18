9fbe9cac4341ff02eae9c74c2ef5fac93abc853e
d4v <dlunna@gmail.com>
2022-05-18 17:01:48 -0500

    cambios del miercoles

--- project/learning/urls.py
+++ project/learning/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views as learning_views
 
 urlpatterns = [
-    path('', learning_views.home, name="learning_levelA1"),    
-    path('<int:page_id>/<slug:page_slug>/', learning_views.page, name="page"),
+    path('', learning_views.home, name="learning_home"),    
+    path('A1', learning_views.A1, name="learning_A1"),        
+    path('A1', learning_views.A1videos, name="learning_A1videos"),        
 ]
 
 
--- project/learning/views.py
+++ project/learning/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def home (request):    
-    return render(request, "learning/nivel.html")
+    return render(request, "learning/nivelA1.html")
 
 #se recupera 1 de acuerdo al ID
-def page(request, page_id, page_slug):
-    objeto = page_id
-    return render(request, 'learning/nivel.html', {'llave':objeto})
+def A1(request):
+    return render(request, 'learning/nivelA1.html')
+
+def A1videos(request):
+    return render(request, 'learning/nivelA1videos.html')
