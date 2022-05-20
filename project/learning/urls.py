832f47b6a59250275ce168259bd6060a3ee53109
d4v <dlunna@gmail.com>
2022-05-20 17:35:13 -0500

    fontAW

--- project/learning/urls.py
+++ project/learning/urls.py
@@ -4,7 +4,7 @@ from . import views as learning_views
 urlpatterns = [
     path('', learning_views.home, name="learning_home"),    
     path('A1', learning_views.A1, name="learning_A1"),        
-    path('A1', learning_views.A1videos, name="learning_A1videos"),        
+    path('A1/videos', learning_views.A1videos, name="learning_A1videos"),        
 ]
 
 
